d12bdfc68f9caa74f9414f4c7ee760443562d013
Ruan Heyns <pasrone56@gmail.com>
2019-07-30 14:34:41 +0200

    New Images, Updated Contact Form, updated Palette

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,7 +11,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "rheynsza@gmail.com"; 
+$to = "http://formspree.io/rheynsza@gmail.com"; 
 $subject = "Portfolio Contact Form:  $name";
 $body = "You have received a new message from your portfolio contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@portfolio.com\n"; 
